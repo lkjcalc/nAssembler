dc237f8db5dc1a668350884edff949e62b8b05f1
lkjcalc <lkjcalc@gmail.com>
2018-02-20 14:21:18 +0100

    make autotests more robust (detect if the assembler doesn't write the binary output or prints an error)

--- tests/autotests.py
+++ tests/autotests.py
@@ -57,17 +57,37 @@ def do_all_tests():
             print("No known source hash for '{}'. Calculating new source and binary hash.".format(fname))
             srchashchanged = True
             setnewhashes = True
+        outfilename = srcdir + fname[:-4] + '.tns'
+        # move old binary
+        try:
+            os.rename(outfilename, outfilename+'.old')
+        except FileNotFoundError:
+            pass
         # assemble file
         fin = open(subproc_stdin_fname, 'w')
         fin.write(srcdir + fname + '\n')
-        fin.write(srcdir + fname[:-4] + '.tns\n')
+        fin.write(outfilename + '\n')
         fin.close()
         fin = open(subproc_stdin_fname)
         fout = open(subproc_stdout_fname, 'w')
         subprocess.call(['python3', 'bundledmain.py'], stdin=fin, stdout=fout)
-        #TODO: should close files?
+        fin.close()
+        fout.close()
+        fout = open(subproc_stdout_fname, 'r')
+        s = fout.read()
+        fout.close()
+        # check stdout
+        if 'Error' in s:
+            print("Failed test. Assembler failed:")
+            print(s)
+            exit(1)
         # check output
-        binmd5 = calculate_md5_of_file(fname[:-4] + '.tns')
+        try:
+            binmd5 = calculate_md5_of_file(fname[:-4] + '.tns')
+        except FileNotFoundError:
+            print("Failed test. No output binary found for '{}'. Assembler stdout:".format(fname))
+            print(s)
+            exit(1)
         try:
             if binmd5 != get_known_md5_of_file(fname[:-4] + '.tns'):
                 binhashchanged = True
@@ -88,8 +108,5 @@ def do_all_tests():
             print("Failed tests.")
             exit(1)
 
-do_all_tests()
 
-src_hash_list = ['36d7985f55521ad32dafbf9623c7da93', 'e06649b7675ce1bfcc61fe13c8d56d6a', '66ea2d57abeec95f56bc6116d35a022b', 'd95f7cac333fbd1732f771cf9f07abb4', 'dd1b84ea2ac9b0be819b4c83cc5e75b3', '18ac487586890f8b32dff351e9fadedb']
-tns_hash_list = ['7977c81268034a0b21daff8a8c913a5e', '16df3dba78cb45e37bb4981415ef74dc', '0977abdea7079e0ce1c4e7f255985f71', 'bf6ec9b5d164f46e0a981cd5e456b0eb', '8a4475ec9444f278de7470d0250c0503', '3187101e4fafaefc9c44915f430a61e5']
-filename_list = ['armasm_test', 'armasmt2', 'boot1largetest', 'armasm_spacet', 'lowercase_test', 'casetest2']
+do_all_tests()
